36d678cecb4534011c7c9f4ae16e8f632796ec7a
Sunday-Gad <sundayzephaniahgad@gmail.com>
2022-10-24 19:15:21 +0000

    correcting errors

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-"""
-more class base
-"""
-
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-"""
-Square class
-"""
-
-
 class Square(Rectangle):
-    """ Square Class """
     def __init__(self, size):
-        """ size init"""
+        self.integer_validator(size, size)
+        super().__init__(size, size)
         self.__size = size
-        super().__init__(self.__size, self.__size)
 
     def __str__(self):
-        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
+        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
